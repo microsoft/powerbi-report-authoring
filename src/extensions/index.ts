79e1db18d7a245ebc12ce444c3a3eeb0e8effc5d
Ali Hamud <alihamud@microsoft.com>
2019-05-16 12:21:57 +0300

    Add TODO for UT

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -3,6 +3,7 @@ import { IPowerBIClientExtension } from "./powerBIClientExtension";
 import { ReportExtensions } from "./reportExtensions";
 import { VisualExtensions } from "./visualExtensions";
 
+// TODO: Add unit tests to all methods in all extensions.
 export const extensions: IPowerBIClientExtension[] = [
     new ReportExtensions(),
     new PageExtensions(),
